f4e55e40c0b0d9d232a074f414c04c8de0cb227f
kylesadler <kylesadler5@gmail.com>
2021-02-10 20:25:54 -0600

    added hilbert matrix

---
+++ vector-and-matrix/hilbert-matrix.c
@@ -0,0 +1,31 @@
+#include "array.h"
+#include <stdio.h>
+
+double** hilbert_matrix(int n) {
+    double** x;
+    make_matrix(x, n, n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            x[i][j] = 1.0 / (1 + i + j);
+        }
+    }
+
+    return x;
+}
+
+int main() {
+    double** H;
+    int n = 8;
+    H = hilbert_matrix(n);
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%f ", H[i][j]);
+        }
+        printf("\n");
+    }
+
+    free_matrix(H);
+    return 0;
+}
