4078d5f83755d3979e9080d44d6832b54cf0339e
kyle sadler <kylesadler5@gmail.com>
2021-02-26 19:19:35 -0600

    added demos

--- nelder-mead/nelder-mead.h
+++ nelder-mead/nelder-mead.h
@@ -1,14 +1,14 @@
 #ifndef H_NELDER_MEAD_H
-#define H_NELDER_MEAD_H
+#define H_NELDER_MEAD_H 
 struct nelder_mead {
 	double (*f) (double *x, int n, void *params); // objective function
-	int n;		// dimension of the space
-	double **s;	// (n+1) x n matrix: the simplex
-	double **x;	// n-vector: the iteration's starting/ending point
-	double h;	// the problem's length scale
-	double tol;	// tolerance; stopping criterion
+	int n;			// dimension of the space
+	double **s;		// (n+1) x n matrix: the simplex
+	double **x;		// n-vector: the iteration's starting/ending point
+	double h;		// the problem's length scale
+	double tol;		// tolerance; stopping criterion
 	int maxevals;	// max number of evaluations
-	double mineval;	// the computed minimym value
+	double mineval;	// the computed minimum value
 	void *params;	// parameters passed to f()
 };
 
