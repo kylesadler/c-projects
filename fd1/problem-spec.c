f24329438d49e2725573c4ee3cc8f40b52b7089d
kyle sadler <kylesadler5@gmail.com>
2021-03-07 19:27:25 -0600

    added heat2

---
+++ fd1/problem-spec.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <math.h>
+#include "problem-spec.h"
+
+// heat1( ): quite a basic 1-D heat equation with exact solution
+static double heat1_exact(double x, double t)
+{
+	double Pi = 4*atan(1);
+	// return exp(-Pi*Pi/4*t) * cos(Pi/2*x);
+	return exp(-Pi*Pi/2*t) * cos(Pi/2*x);
+}
+
+static double heat1_ic(double x)
+{
+	return heat1_exact(x,0);
+}
+
+static double heat1_bcL(double t)
+{
+	return heat1_exact(-1,t);
+}
+
+static double heat1_bcR(double t)
+{
+	return heat1_exact(1,t);
+}
+
+struct problem_spec *heat1(void)
+{
+	static struct problem_spec spec = {	// C99-style initialization!
+		.a		= -1.0,
+		.b		=  1.0,
+		.ic		= heat1_ic,
+		.bcL		= heat1_bcL,
+		.bcR		= heat1_bcR,
+		.u_exact	= heat1_exact,
+	};
+	printf("problem heat1:\n");
+	return &spec;
+}
+
+
+
+
+/* not valid for negative t */
+static double heat2_exact(double x, double t)
+{
+    if (t > 0) {
+        return erf(x/2/sqrt(t));
+    } else if (x < 0) {
+        return -1;
+    } else if (x > 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+static double heat2_ic(double x)
+{
+	return heat2_exact(x,0);
+}
+
+static double heat2_bcL(double t)
+{
+	return heat2_exact(-1,t);
+}
+
+static double heat2_bcR(double t)
+{
+	return heat2_exact(1,t);
+}
+
+struct problem_spec *heat2(void)
+{
+	static struct problem_spec spec = {	// C99-style initialization!
+		.a		= -1.0,
+		.b		=  1.0,
+		.ic		= heat2_ic,
+		.bcL		= heat2_bcL,
+		.bcR		= heat2_bcR,
+		.u_exact	= heat2_exact,
+	};
+	printf("problem heat2:\n");
+	return &spec;
+}
+
