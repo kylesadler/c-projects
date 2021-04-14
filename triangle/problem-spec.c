d254539094321c99995088467f79ab54cb0492d2
kylesadler <kylesadler5@gmail.com>
2021-04-13 19:28:51 -0500

    update problem spec

--- triangle/problem-spec.c
+++ triangle/problem-spec.c
@@ -1,3 +1,7 @@
+#include "problem_spec.h"
+#include "array.h"
+#include "xmalloc.h"
+
 struct problem_spec *triangle_with_hole(void) {
     static struct problem_spec_point points[] = {
         // triangle vertices
@@ -45,5 +49,57 @@ struct problem_spec *triangle_with_hole(void) {
 
     printf("domain is a triangle with hole\n");
     return &spec;
-    
+
 }
+
+struct problem_spec *annulus(int n) {
+
+    double Pi = 4*atan(1);
+    double a = 0.325, b = 2*a;
+    struct problem_spec *spec = xmalloc(sizeof *spec);
+    make_vector(spec->points, 2*n);
+    make_vector(spec->segments, 2*n);
+    make_vector(spec->holes, 1);
+
+    for (int i = 0; i < n; i++) {
+        double t = 2*i*Pi/n;
+
+        spec->points[i].point_no = i;
+        spec->points[i].x = a*cos(t);
+        spec->points[i].y = a*sin(t);
+        spec->points[i].bc = FEM_BC_DIRICHLET;
+        spec->points[i+n].point_no = i+n;
+        spec->points[i+n].x = b*cos(t);
+        spec->points[i+n].y = b*sin(t);
+        spec->points[i+n].bc = FEM_BC_DIRICHLET;
+    }
+
+    for (int i = 0; i < n; i++) {
+        double t = 2*i*Pi/n;
+
+        spec->segments[i].segment_no = i;
+        spec->segments[i].point_no_1 = i;
+        spec->segments[i].point_no_2 = i+1;
+        spec->segments[i].bc = FEM_BC_DIRICHLET;
+        spec->segments[i+n].segment_no = i+n;
+        spec->segments[i+n].point_no_1 = i+n;
+        spec->segments[i+n].point_no_2 = i+n+1;
+        spec->segments[i+n].bc = FEM_BC_DIRICHLET;
+    }
+
+    spec->segments[n-1].point_no_2 -= n;
+    spec->segments[2*n-1].point_no_2 -= n;
+
+    spec->holes[0].x = spec->holes[0].y = 0.0;
+
+    spec->npoints = 2*n;
+    spec->nsegments = 2*n;
+    spec->nholes = 1;
+    spec->f = spec->g = spec->h = spec->eta = spec->u_exact = NULL;
+
+    printf("domain is an annulus (really a %d-gon) or radii %g and %g\n", n, a, b);
+    return spec;
+
+}
+
+
